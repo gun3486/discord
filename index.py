a824d179910b65720e5b964cccf86137c6256e03
임준명 <73639169+gun3486@users.noreply.github.com>
2021-08-15 01:18:48 +0900

    디스코드봇테스트
    봇테스트

--- index.py
+++ index.py
@@ -1,16 +1,34 @@
+from asyncio.tasks import sleep
 import discord
 import asyncio
 import sys
+import threading, time
+import schedule
+import datetime
 
 client = discord.Client()
 
 token = sys.argv[1]
 print(token)
 
-@client.event
-async def on_ready():
-    print('hello')
-    channel = client.get_channel(855854595585343498)
-    await channel.send('test1')
+async def loop():
+    time.sleep(3)
+    while True:
+        channel = client.get_channel(855854595585343498)
+        await channel.send("지금은 유리시입니다.")
+        time.sleep(1)
+
+async def callback():
+    await loop()
+
+def between_callback():
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    loop.
+    loop.run_until_complete(callback())
+    loop.close()
+
+thread = threading.Thread(target=between_callback)
+thread.start()
 
 client.run(token)
