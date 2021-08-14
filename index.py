d6b89fac12e36b71ce78c14faa8e37b47be73b36
임준명 <73639169+gun3486@users.noreply.github.com>
2021-08-14 23:53:18 +0900

    디스코드봇
    디스코드봇 메시지 출력

--- index.py
+++ index.py
@@ -1,6 +1,16 @@
 import discord
 import asyncio
+import sys
 
-client = discord.Cilent()
+client = discord.Client()
 
-token = ""
+token = sys.argv[1]
+print(token)
+
+@client.event
+async def on_ready():
+    print('hello')
+    channel = client.get_channel(855854595585343498)
+    await channel.send('test1')
+
+client.run(token)
